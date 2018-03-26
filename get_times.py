dbcee92bc3a2aa16dd025073b4c4ef0405e4a197
jessemcbrower <browerpowr@gmail.com>
2018-03-25 22:37:52 -0400

    added loop to scroll function based on length

--- get_times.py
+++ get_times.py
@@ -1,10 +1,13 @@
 import time
 from predict import predict
-
-# USE THIS ON THE PHAT SIDE TO SEE LIGHTS
-
 import scrollphathd as sphd
+
+# SETS DEFAULT BRIGHTNESS
+
 sphd.set_brightness(0.2)
+
+# ROTATES DISPLAY 180 DEGREES
+
 sphd.rotate(180)
 
 stops = [
@@ -19,7 +22,10 @@ for s in stops:
 
 time.sleep(1)
 
-def get_times():
+	# GETS TIMES OF NEXT BUSES PER ROUTE
+
+while True:
+
 	currentTime = time.time()
 	nxt_times = []
 	for pl in predictList:
@@ -29,50 +35,31 @@ def get_times():
 				t = p - (currentTime - pl.lastQueryTime)
 				nxt_times.append(str(int(t/60)))
 
-		# USE THIS ON THE MAC SIDE FOR TESTING IN THE CONSOLE
+		# IF NO BUSES ARE COMING
 
-		# print 'The next ' + bus_num + ' bus will arrive in: '
-		
 		if len(nxt_times) == 0:
 
-			# USE THIS ON THE PHAT SIDE TO SEE LIGHTS
+			message = '      No buses today.      '
 
-			string = '     No buses today.     '
-			# sphd.write_string(string)
+		# IF ONE BUS IS COMING
 
 		elif len(nxt_times) == 1:
-			
-			# USE THIS ON THE PHAT SIDE TO SEE LIGHTS
 
-			string = '     The next ' + bus_num + ' bus arrives in: ' + str(nxt_times[0]) + " minutes.     "
-			# sphd.write_string(string)
+			message = '      The next ' + bus_num + ' bus arrives in: ' + str(nxt_times[0]) + ' minutes.      '
 
-			# USE THIS ON THE MAC SIDE FOR TESTING IN THE CONSOLE
-
-			# print str(nxt_times[0]) + " minutes."
+		# IF MULTIPLE BUSES ARE COMING
 
 		elif len(nxt_times) > 1:
 
-			# USE THIS ON THE PHAT SIDE TO SEE LIGHTS
-			
-			string = '     The next ' + bus_num + ' buses arrive in: ' + (', '.join(str(e) for e in nxt_times[0:-1])) + ' and ' + str(nxt_times[-1]) + ' minutes.     '
-			# sphd.write_string(string)
+			message = '      The next ' + bus_num + ' buses arrive in: ' + (', '.join(str(e) for e in nxt_times[0:-1])) + ' and ' + str(nxt_times[-1]) + ' minutes.      '
 
-			# USE THIS ON THE MAC SIDE FOR TESTING IN THE CONSOLE
+	sphd.write_string(message)
 
-			# print (', '.join(str(e) for e in nxt_times[0:-1])) + ' and ' + str(nxt_times[-1]) + ' minutes.'
-
-			# USE THIS ON THE MAC SIDE FOR TESTING IN THE CONSOLE
-
-			# print 'No buses today.'
-
-	# sphd.show()
-	# sphd.scroll()
-	
-	prevTime = currentTime
-	sphd.write_string(string)
+	for i in range(sphd.get_buffer_shape()[0]):
+		sphd.show()
+		sphd.scroll()
+	sphd.clear()
 	sphd.show()
-	sphd.scroll()
+	time.sleep(2)
 
-while True:
-	get_times()
+	prevTime = currentTime
